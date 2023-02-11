cbff51a8378d2a3087b3750358424785a1510b9c
dani7514 <106800350+dani7514@users.noreply.github.com>
2023-02-11 23:05:00 +0300

    Time: 371 ms (27.14%), Space: 15.2 MB (74.57%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        s_dict = collections.Counter(s[:len(p)-1]) 
+        p_dict = collections.Counter(p)
+        start = 0
+        res = []
+        for i in range(len(p)-1, len(s)):
+            s_dict[s[i]] += 1
+            if s_dict == p_dict:
+                res.append(start)
+            s_dict[s[start]] -= 1
+            if s_dict[s[start]] == 0:
+                del s_dict[s[start]]
+            start += 1
+            
+        return res
+        
