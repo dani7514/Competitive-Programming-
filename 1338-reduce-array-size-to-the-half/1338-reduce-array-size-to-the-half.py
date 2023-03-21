7e9497bcef6b79cd03927553bf39d697a3206782
dani7514 <106800350+dani7514@users.noreply.github.com>
2023-03-21 23:09:18 +0300

    Time: 696 ms (27.28%), Space: 31.8 MB (73.97%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,12 +1,20 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         arr.sort()
+        dic={}
+        tmpArr=[]
         tmpSum=0
         count=0
-        dic = Counter(arr)
-        dic = dict(sorted(dic.items(), key = lambda item: item[-1], reverse =True))
-        for i in dic:
-            tmpSum+=dic[i]
+        for i in arr:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        for j in dic:
+            tmpArr.append(dic[j])
+        tmpArr.sort()
+        for i in range(len(tmpArr)-1,-1,-1):
+            tmpSum+=tmpArr[i]
             count+=1
             if tmpSum>=len(arr)//2:
                 return count
