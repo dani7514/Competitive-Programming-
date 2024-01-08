4f5feff8d556952417737d8209864b33211a5e0f
Daniel Misganaw <106800350+dani7514@users.noreply.github.com>
2024-01-08 21:55:13 +0300

    Added solution for Sort Colors

--- leet-code-solutions/sort-colors.py
+++ leet-code-solutions/sort-colors.py
@@ -1,14 +1,19 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        n = len(nums)
-        for i in range(n):
-            min_index = i
-            for j in range(i+1, n):
-                if nums[j] < nums[min_index]:
-                    min_index = j
-            nums[i], nums[min_index] = nums[min_index], nums[i]
-        # return nums
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+class Solution(object):
+    def sortColors(self, nums):
+        left= 0 
+        curr = 0 
+        right = len(nums)-1
+        while curr <= right:
+            if nums[curr] == 0:
+                nums[left],nums[curr]= nums[curr],nums[left] 
+                left += 1
+                curr += 1 
+                
+            elif nums[curr] == 2:
+                nums[curr],nums[right]  = nums[right],nums[curr]
+                right -= 1 
+                
+            else:
+                curr += 1
+        return nums
+            
