e9a7212b0230977ca208320909eb760735d39f36
Daniel Misganaw <106800350+dani7514@users.noreply.github.com>
2024-02-19 15:47:39 +0300

    Add solution for Next Greater Element I

---
+++ leetcode/next-greater-element-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nextGreaterDict = {}
+        monoStack = []
+        
+        for num in nums2:
+            while monoStack and num > monoStack[-1]:
+                nextGreaterDict[monoStack.pop()] = num
+            monoStack.append(num)
+                
+        for i, num in enumerate(nums1):
+            nums1[i] = nextGreaterDict.get(num, -1)
+            
+        return nums1
+        
